172759070f2a1b105e3e67fe7e8c7d202853e752
Syllina <syllinxia@gmail.com>
2024-11-12 23:02:52 +0800

    add next.js demo

---
+++ frontend/demo1/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { HStack } from "@chakra-ui/react"
+
+export default function Home() {
+  return (
+    <HStack>
+      <Button> Click me </Button>
+      <Button> Click me </Button>
+    </HStack>
+  );
+}
